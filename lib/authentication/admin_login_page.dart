cf0572a5fea8184b94649a64cf94b7272ca877e9
Szymon Rosowski <rosowskiszymon@gmail.com>
2023-08-10 19:17:35 +0200

    changed login to private auth server, added working mfa

--- lib/authentication/admin_login_page.dart
+++ lib/authentication/admin_login_page.dart
@@ -1,5 +1,9 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:raportowanie_zdarzen_niebezpiecznych/authentication/secrets/api_key.dart';
+import 'package:http/http.dart' as http;
 
 class AdminLoginPage extends StatefulWidget {
   const AdminLoginPage({super.key});
@@ -12,6 +16,9 @@ class _AdminLoginPageState extends State<AdminLoginPage> {
   final _formKey = GlobalKey<FormState>();
   final _emailController = TextEditingController();
   final _passwordController = TextEditingController();
+  final _totpController = TextEditingController();
+  final FocusNode _focusNode1 = FocusNode();
+  final FocusNode _focusNode2 = FocusNode();
   bool _isLoading = false;
 
   @override
@@ -22,7 +29,7 @@ class _AdminLoginPageState extends State<AdminLoginPage> {
           width: MediaQuery.of(context).size.height /
                       MediaQuery.of(context).size.width <
                   1
-              ? MediaQuery.of(context).size.width * 0.50
+              ? MediaQuery.of(context).size.width * 0.40
               : double.infinity,
           child: Card(
             child: Padding(
@@ -44,6 +51,9 @@ class _AdminLoginPageState extends State<AdminLoginPage> {
                           }
                           return null;
                         },
+                        focusNode: _focusNode1,
+                        onFieldSubmitted: (_) =>
+                            FocusScope.of(context).requestFocus(_focusNode2),
                       ),
                     ),
                     Padding(
@@ -58,6 +68,8 @@ class _AdminLoginPageState extends State<AdminLoginPage> {
                           }
                           return null;
                         },
+                        focusNode: _focusNode2,
+                        onFieldSubmitted: (_) => _signIn(),
                       ),
                     ),
                     const SizedBox(height: 16),
@@ -85,14 +97,78 @@ class _AdminLoginPageState extends State<AdminLoginPage> {
     if (_formKey.currentState!.validate()) {
       setState(() => _isLoading = true);
       try {
-        await FirebaseAuth.instance.signInWithEmailAndPassword(
-          email: _emailController.text,
-          password: _passwordController.text,
-        );
-        Navigator.of(context).pushReplacementNamed('/admin-panel');
-      } on FirebaseAuthException catch (e) {
+        var response = await http.post(Uri.parse('$API_URL/verify-user'),
+            body: json.encode({
+              'api_key': API_KEY,
+              'email': _emailController.text,
+              'password': _passwordController.text,
+            }));
+        if (response.statusCode == 200) {
+          showDialog(
+              context: context,
+              builder: (context) {
+                return AlertDialog(
+                  title: const Text('Weryfikacja Dwuetapowa'),
+                  content: SizedBox(
+                    width: 300,
+                    height: 100,
+                    child: Column(
+                      children: [
+                        TextFormField(
+                          decoration:
+                              const InputDecoration(labelText: 'Kod 2FA'),
+                          controller: _totpController,
+                          maxLength: 6,
+                          obscureText: false,
+                          validator: (value) {
+                            if (value == null || value.isEmpty) {
+                              return 'Proszę wprowadzić kod 2FA';
+                            }
+                            return null;
+                          },
+                        ),
+                      ],
+                    ),
+                  ),
+                  actions: [
+                    TextButton(
+                        onPressed: () {
+                          http
+                              .post(
+                                Uri.parse('$API_URL/verify-totp'),
+                                headers: {'Content-Type': 'application/json'},
+                                body: json.encode({
+                                  'api_key': API_KEY,
+                                  'email': _emailController.text,
+                                  'password': _passwordController.text,
+                                  'totp': _totpController.text,
+                                }),
+                              )
+                              .then(
+                                  (value) => Navigator.of(context).pop(value));
+                        },
+                        child: const Text('Zwerfikuj')),
+                  ],
+                );
+              }).then((value) async {
+            try {
+              print(value.body);
+              await FirebaseAuth.instance.signInWithCustomToken(value.body);
+              Navigator.of(context).popAndPushNamed('/admin-panel');
+            } catch (e) {
+              ScaffoldMessenger.of(context).showSnackBar(
+                const SnackBar(content: Text('Wprowadzono nieprawidłowy kod 2FA')),
+              );
+            }
+          });
+        } else {
+          ScaffoldMessenger.of(context).showSnackBar(
+            const SnackBar(content: Text('Nieprawidłowy email lub hasło')),
+          );
+        }
+      } catch (e) {
         ScaffoldMessenger.of(context).showSnackBar(
-          SnackBar(content: Text(e.message ?? 'Wystąpił błąd')),
+          const SnackBar(content: Text('Wystąpił błąd podczas logowania')),
         );
       } finally {
         setState(() => _isLoading = false);
